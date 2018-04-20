c93f543cb2f80a3683d331d9c73c51f41c251a60
wilsonj24 <31478966+wilsonj24@users.noreply.github.com>
2018-04-20 14:13:58 -0400

    Added code to labten

---
+++ src/main/java/labten/experiment/Experiment.java
@@ -0,0 +1,18 @@
+package labten.experiment;
+
+import labten.computation.Cryptography;
+import labten.data.ResultsTable;
+
+/**
+ * An experiment class that can run a campaign of experiments.
+ * Note that this supports running campaigns of experiments for a
+ * differnt number of rounds depending on the strategy.
+ *
+ * @author Jordan Wilson, Ben Watto, Robert Samuel, and Dillon Thoma
+ */
+
+public class Experiemnt {
+
+  /** The campaign length for the Cryptography. */
+  private static final int CRYPTOGRAPHY_CAMPAIGN_LENGTH = 6;
+}
