d24d9bc53e19a92133a3dd0e82477365934440a9
Robert Samuel <robertsamuel@Roberts-MacBook.local>
2018-04-20 14:03:03 -0400

    Final Project

--- src/main/java/labten/data/ResultsTable.java
+++ src/main/java/labten/data/ResultsTable.java
@@ -9,4 +9,21 @@ import java.util.Arrays;
 public class ResultsTable {
 
   private long[][] results;
+
+  private int currentRow;
+
+  private static final int FIRST_ROW = 0;
+
+  private static final int COLUMN_NUMBER = 3;
+
+  private static final int SIZE = 0;
+
+  private static final int TIMING = 1;
+
+  private static final int RATIO = 2;
+
+
+  public ResultsTable(int rowCount) {
+    results = new long [rowCount][COLUMN_NUMBER]
+  }
 }
