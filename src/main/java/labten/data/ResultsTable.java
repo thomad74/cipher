21f5d43d0048f2ee3f7f420d0a34c653426671c2
Robert Samuel <robertsamuel@Roberts-MacBook.local>
2018-04-20 13:51:32 -0400

    Final Project

---
+++ src/main/java/labten/data/ResultsTable.java
@@ -0,0 +1,12 @@
+package labten.data;
+
+import java.util.Arrays;
+
+/**
+* A results table that stores data from running a timing experiment
+*/
+
+public class ResultsTable {
+
+  private long[][] results;
+}
