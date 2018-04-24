42e691ed495943fe4bca568f476f3646886740ba
Benjamin Watto <wattob@aldenv126.allegheny.edu>
2018-04-24 16:12:12 -0400

    implementing variables for runcampaign

--- src/main/java/labten/experiment/RunCampaign.java
+++ src/main/java/labten/experiment/RunCampaign.java
@@ -1,16 +1,30 @@
-// package labten.experiment;
-//
-// import labten.computation.Cryptography;
-// import labten.data.ResultsTable;
-//
-// import java.util.Random;
-//
-// /**
-//   * A RunCampaign that can run an experiment with a computer.
-//   *
-//   * @author Dillon Thoma, Ben Watto, Jordan Wilson, Robert Samuel
-//   */
-//
-// public class RunCampaign implements Campaign {
-//
-// }
+package labten.experiment;
+
+import labten.computation.Cryptography;
+import labten.data.ResultsTable;
+
+import java.util.Random;
+
+/**
+  * A RunCampaign that can run an experiment with a computer.
+  *
+  * @author Dillon Thoma, Ben Watto, Jordan Wilson, Robert Samuel
+  */
+
+public class RunCampaign implements Campaign {
+
+  /** The starting size for running an experiment campaign. */
+  private static final int INPUT_SIZE_START = 250;
+
+  /** The growth factor for input when running an experiment campaign. */
+  private static final int INPUT_GROWTH_FACTOR = 2;
+
+  /** The total number of input size doubles to perform for an experiment campaign. */
+  private static final int CAMPAIGN_LENGTH = 10;
+
+  /** The indentation level for the output. */
+  private static final String IDENTATION = "  ";
+
+  static final String ValidInputs = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+}
