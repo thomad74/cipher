7bdafff78861014e3dcca909f8e497bc59608008
Benjamin Watto <wattob@aldenv126.allegheny.edu>
2018-04-24 14:38:26 -0400

    updated campaign

---
+++ src/main/java/labten/experiment/Campaign.java
@@ -0,0 +1,18 @@
+package labten.experiment;
+
+import labten.data.ResultsTable;
+
+/**
+ * A Campaign interface used to run an experiment.
+ *
+ * @author
+ */
+
+public interface Campaign {
+
+  /** 
+   *
+   */
+  public ResultsTable run(, int campaignLength);
+
+}
