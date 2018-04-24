165d6193cc1fe54aeb493a872de2582e63d2bfc5
Dillon Thoma <thomad@aldenv127.allegheny.edu>
2018-04-24 15:37:13 -0400

    test cryptography first test case

--- src/main/java/labten/experiment/RunCampaign.java
+++ src/main/java/labten/experiment/RunCampaign.java
@@ -1,16 +1,16 @@
-package labten.experiment;
-
-import labten.computation.Cryptography;
-import labten.data.ResultsTable;
-
-import java.util.Random;
-
-/**
-  * A RunCampaign that can run an experiment with a computer.
-  *
-  * @author Dillon Thoma, Ben Watto, Jordan Wilson, Robert Samuel
-  */
-
-public class RunCampaign implements Campaign {
-  
-}
+// package labten.experiment;
+//
+// import labten.computation.Cryptography;
+// import labten.data.ResultsTable;
+//
+// import java.util.Random;
+//
+// /**
+//   * A RunCampaign that can run an experiment with a computer.
+//   *
+//   * @author Dillon Thoma, Ben Watto, Jordan Wilson, Robert Samuel
+//   */
+//
+// public class RunCampaign implements Campaign {
+//
+// }
--- src/test/java/labten/TestCryptography.java
+++ src/test/java/labten/TestCryptography.java
@@ -1,9 +1,17 @@
 package labten;
 
-// import static org.junit.Assert.assertArrayEquals;
-// import static org.junit.Assert.assertEquals;
-// import static org.junit.Assert.assertTrue;
+// import org.junit.Assert.assertArrayEquals;
+// import org.junit.Assert.assertEquals;
+// import org.junit.Assert.assertTrue;
+
+import labten.Cryptography;
+// import org.junit.test;
 
 public class TestCryptography {
 
+@Test
+  public void cipherCanEncode() {
+    String message = encryptedMsg.getKey().substring(0, 10);
+    assertEquals(message, encryptedMsg.encode("aaaaaaaaaa"));
+  }
 }
