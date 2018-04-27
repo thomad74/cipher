68e8269341e9efa0deb6641e64e14bfba927c3de
Benjamin Watto <wattob@aldenv186.allegheny.edu>
2018-04-27 17:25:36 -0400

    added caesar test

---
+++ src/test/java/labten/TestCaesarCipher.java
@@ -0,0 +1,30 @@
+package labten;
+
+// import org.junit.Assert.assertArrayEquals;
+// import org.junit.Assert.assertEquals;
+// import org.junit.Assert.assertTrue;
+
+import labten.cipher.CaesarCipher;
+// import org.junit.test;
+
+public class TestCaesarCipher {
+
+  /** The maximum number of items to use for testing. */
+  private static int MAXIMUM_NUMBER_ITEMS = 100;
+
+  /** The maximum number of items to use for testing. */
+  private static int MAXIMUM_NUMBER_CHARS = 43;
+
+  /** The size of the alphabet for character generation. */
+  private static int ALPHABET_SIZE = 26;
+
+  /** The starting character. */
+  private static char STARTING_CHARACTER = 'a';
+
+//
+// @Test
+//   public void cipherCanEncode() {
+//     String message = encryptedMsg.getKey().substring(0, 10);
+//     assertEquals(message, encryptedMsg.encode("aaaaaaaaaa"));
+//   }
+}
