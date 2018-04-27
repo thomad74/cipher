ed2200db772d6e515a7fd5ec13edc01ee5c605ef
Benjamin Watto <wattob@aldenv186.allegheny.edu>
2018-04-27 17:25:27 -0400

    added vigenere test

---
+++ src/test/java/labten/TestVigenereCipher.java
@@ -0,0 +1,32 @@
+package labten;
+
+//import static org.junit.Assert.assertArrayEquals;
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertTrue;
+
+import labten.cipher.VigenereCipher;
+//import org.junit.Test;
+
+import java.util.Random;
+
+/**
+ * A JUnit test suite for the VigenereCipher.
+ *
+ * @author
+ */
+
+public class TestVigenereCipher {
+
+  /** The maximum number of items to use for testing. */
+  private static int MAXIMUM_NUMBER_ITEMS = 100;
+
+  /** The maximum number of items to use for testing. */
+  private static int MAXIMUM_NUMBER_CHARS = 43;
+
+  /** The size of the alphabet for character generation. */
+  private static int ALPHABET_SIZE = 26;
+
+  /** The starting character. */
+  private static char STARTING_CHARACTER = 'a';
+
+}
