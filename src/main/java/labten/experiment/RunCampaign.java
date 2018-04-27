920a78c6dd279c9240f141d326314cdda7c48b7d
Benjamin Watto <wattob@aldenv186.allegheny.edu>
2018-04-27 16:32:05 -0400

    changes to run campaign and todo

--- src/main/java/labten/experiment/RunCampaign.java
+++ src/main/java/labten/experiment/RunCampaign.java
@@ -1,6 +1,6 @@
 package labten.experiment;
 
-import labten.computation.Cryptography;
+import labten.cipher.Cipher;
 import labten.data.ResultsTable;
 
 import java.util.Random;
@@ -13,27 +13,50 @@ import java.util.Random;
 
 public class RunCampaign implements Campaign {
 
-  /** The starting size for running an experiment campaign. */
-  private static final int INPUT_SIZE_START = 250;
+    /** The starting size for running an experiment campaign. */
+    private static final int INPUT_SIZE_START = 250;
 
-  /** The growth factor for input when running an experiment campaign. */
-  private static final int INPUT_GROWTH_FACTOR = 2;
+    /** The growth factor for input when running an experiment campaign. */
+    private static final int INPUT_GROWTH_FACTOR = 2;
 
-  /** The total number of input size doubles to perform for an experiment campaign. */
-  private static final int CAMPAIGN_LENGTH = 10;
+    /** The total number of input size doubles to perform for an experiment campaign. */
+    private static final int CAMPAIGN_LENGTH = 10;
 
-  /** The indentation level for the output. */
-  private static final String IDENTATION = "  ";
+    /** The indentation level for the output. */
+    private static final String IDENTATION = "  ";
 
-  static final String ValidInputs = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
-  public String generateRandomString(int size) {
-    StringBuilder sb = new StringBuilder(size);
-    Random random = new Random();
-    for (int i = 0; i < size; i++) {
-      sb.append(ValidInputs.charAt(random.nextInt(ValidInputs.length())));
+    /** Randomly generate data to use for an experiment. */
+    public int[] generateRandomData(int size) {
+      Random random = new Random();
+      int[] generated = new int[size];
+      //TODO: use a stringbuilder to create an array of char/strings
+      for (int i = 0; i < size; i++) {
+        size = (random.nextInt());
+      }
+      return generated;
     }
-    return sb.toString();
-  }
+
+    /** Run the cipher in an experiment campaign. */
+    public ResultsTable run(Cipher cipher) {
+      int campaignRound = 0;
+      int currentInputSize = INPUT_SIZE_START;
+      ResultsTable results = new ResultsTable(CAMPAIGN_LENGTH);
+      System.out.println("Starting a campaign of experiments with " + cipher.getName() + " ...");
+      while (campaignRound < CAMPAIGN_LENGTH) {
+        int[] arraySize = generateRandomData(currentInputSize);
+        long timeBefore = System.currentTimeMillis();
+        cipher.cipher(arraySize);
+        long timeAfter = System.currentTimeMillis();
+        long timeElapsed = timeAfter - timeBefore;
+        results.addResult((long)currentInputSize, timeElapsed);
+        System.out.println(IDENTATION + "Running round " + campaignRound
+            + " with input size " + currentInputSize);
+        currentInputSize = currentInputSize * INPUT_GROWTH_FACTOR;
+        campaignRound++;
+
+      }
+      System.out.println("... Finishing a campaign of experiments with " + cipher.getName());
+      return results;
+    }
 
 }
