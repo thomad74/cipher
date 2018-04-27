0474b76669fc568fb323605531a6866f47a3f280
Benjamin Watto <wattob@aldenv186.allegheny.edu>
2018-04-27 16:00:56 -0400

    cipher class

---
+++ src/main/java/labten/cipher/Cipher.java
@@ -0,0 +1,30 @@
+package labten.cipher;
+
+/**
+ * An abstract class for all of the sorting implementations.
+ *
+ * @author
+ */
+
+public abstract class Cipher {
+
+  /** The name of the Cipher. */
+  protected String name;
+
+  /** The default name of the sorter. */
+  private static final String DEFAULT_CIPHER = "default";
+
+  /** Configure the name of the sorter. */
+  public Cipher() {
+    name = DEFAULT_CIPHER;
+  }
+
+  /** The name of the sorter. */
+  public String getName() {
+    return name;
+  }
+
+  /** Require that a Sorter provide a method for char[] arrays. */
+  public char[] cipher(char[] source);
+
+}
