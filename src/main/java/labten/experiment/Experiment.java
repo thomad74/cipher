5660545f63bde7697d5087e625c0367057d9c582
Benjamin Watto <wattob@aldenv186.allegheny.edu>
2018-04-27 16:31:31 -0400

    edits to experiment

--- src/main/java/labten/experiment/Experiment.java
+++ src/main/java/labten/experiment/Experiment.java
@@ -1,26 +1,37 @@
 package labten.experiment;
 
-import labten.computation.Cryptography;
+import labten.cipher.Caesar;
+import labten.cipher.Cipher;
+import labten.cipher.Vigenere;
 import labten.data.ResultsTable;
 
 /**
  * An experiment class that can run a campaign of experiments.
  * Note that this supports running campaigns of experiments for a
- * differnt number of rounds depending on the strategy.
+ * different number of rounds depending on the strategy.
  *
  * @author Jordan Wilson, Ben Watto, Robert Samuel, and Dillon Thoma
  */
 
 public class Experiment {
 
-  /** Run Cryptography in an experiment campaign. */
   public static void main(String[] args) {
-    RunCampaign runCampaign = new RunCampaign();
-    Cryptography cryptography = new Cryptography();
-    ResultsTable resultsTable = runCampaign.run(computation, campaignLength);
+    RunCampaign runCipherCampaign = new RunCampaign();
+
+    // Create a Caesar cipher and run it in a campaign
+    CaesarCipher caesarCipher = new CaesarCipher();
+    ResultsTable caesarCipherResultsTable = runCipherCampaign.run(caesarCipher);
     System.out.println();
-    System.out.println("Results of an experiment campaign: \n");
-    System.out.println(resultsTable.toString());
+    System.out.println("Results of an experiment campaign with " + caesarCipher.getName() + ":\n");
+    System.out.println(caesarCipherResultsTable.toString());
+    System.out.println();
+
+    // Create a Vigenere Cipher and run it in a campaign
+    VigenereCipher vigenereCipher = new VigenereCipher();
+    ResultsTable vigenereCipherResultsTable = runCipherCampaign.run(vigenereCipher);
+    System.out.println();
+    System.out.println("Results of an experiment campaign with " + vigenereCipher.getName() + ":\n");
+    System.out.println(vigenereCipherResultsTable.toString());
     System.out.println();
   }
 
