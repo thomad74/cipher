09c53229fa02914e708dbe6f519bfc23581c8574
Dillon Thoma <thomad@aldenv178.allegheny.edu>
2018-05-02 19:46:56 -0400

    fixing checkstyle in RunCampaign.java

--- src/main/java/labten/experiment/RunCampaign.java
+++ src/main/java/labten/experiment/RunCampaign.java
@@ -13,62 +13,62 @@ import java.util.Random;
 
 public class RunCampaign implements Campaign {
 
-    /** The starting size for running an experiment campaign. */
-    private static final int INPUT_SIZE_START = 250;
+  /** The starting size for running an experiment campaign. */
+  private static final int INPUT_SIZE_START = 250;
 
-    /** The growth factor for input when running an experiment campaign. */
-    private static final int INPUT_GROWTH_FACTOR = 2;
+  /** The growth factor for input when running an experiment campaign. */
+  private static final int INPUT_GROWTH_FACTOR = 2;
 
-    /** The total number of input size doubles to perform for an experiment campaign. */
-    private static final int CAMPAIGN_LENGTH = 10;
+  /** The total number of input size doubles to perform for an experiment campaign. */
+  private static final int CAMPAIGN_LENGTH = 10;
 
-    /** The indentation level for the output. */
-    private static final String IDENTATION = "  ";
+  /** The indentation level for the output. */
+  private static final String IDENTATION = "  ";
 
-    static final String Inputs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  static final String Inputs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    /** Randomly generate data to use for an experiment. */
-    public String generateRandomData(int size) {
-      StringBuilder sb = new StringBuilder(size);
-      Random random = new Random();
-      for (int i = 0; i < size; i++) {
-        sb.append(Inputs.charAt(random.nextInt(Inputs.length())));
-      }
-
-
-      return sb.toString();
+  /** Randomly generate data to use for an experiment. */
+  public String generateRandomData(int size) {
+    StringBuilder sb = new StringBuilder(size);
+    Random random = new Random();
+    for (int i = 0; i < size; i++) {
+      sb.append(Inputs.charAt(random.nextInt(Inputs.length())));
     }
 
-    /** Run the cipher in an experiment campaign. */
-    public ResultsTable run(Cipher cipher) {
-      int campaignRound = 0;
-      char currentInputSize = INPUT_SIZE_START;
-      ResultsTable results = new ResultsTable(CAMPAIGN_LENGTH);
 
-      System.out.println("Starting a campaign of experiments with " + cipher.getName() + " ...");
-      while (campaignRound < CAMPAIGN_LENGTH) {
-        // TODO: Generate the input array of the required size
+    return sb.toString();
+  }
+
+  /** Run the cipher in an experiment campaign. */
+  public ResultsTable run(Cipher cipher) {
+    int campaignRound = 0;
+    char currentInputSize = INPUT_SIZE_START;
+    ResultsTable results = new ResultsTable(CAMPAIGN_LENGTH);
+
+    System.out.println("Starting a campaign of experiments with " + cipher.getName() + " ...");
+    while (campaignRound < CAMPAIGN_LENGTH) {
+      // TODO: Generate the input array of the required size
 
 
 
-        long timeBefore = System.currentTimeMillis();
-        //cipher.cipher(arraySize);
-        long timeAfter = System.currentTimeMillis();
-        long timeElapsed = timeAfter - timeBefore;
+      long timeBefore = System.currentTimeMillis();
+      //cipher.cipher(arraySize);
+      long timeAfter = System.currentTimeMillis();
+      long timeElapsed = timeAfter - timeBefore;
 
-        // Add the result to the instance of the ResultsTable
-        results.addResult((long)currentInputSize, timeElapsed);
-        System.out.println(IDENTATION + "Running round " + campaignRound
-            + " with input size " + currentInputSize);
+      // Add the result to the instance of the ResultsTable
+      results.addResult((long)currentInputSize, timeElapsed);
+      System.out.println(IDENTATION + "Running round " + campaignRound
+          + " with input size " + currentInputSize);
 
-        // Grow to the next size for the input
+      // Grow to the next size for the input
       //  currentInputSize = currentInputSize * INPUT_GROWTH_FACTOR;
 
-        // Go to the next round of the experiment campaign
-        campaignRound++;
-      }
-      System.out.println("... Finishing a campaign of experiments with " + cipher.getName());
-      return results;
+      // Go to the next round of the experiment campaign
+      campaignRound++;
     }
+    System.out.println("... Finishing a campaign of experiments with " + cipher.getName());
+    return results;
+  }
 
 }
