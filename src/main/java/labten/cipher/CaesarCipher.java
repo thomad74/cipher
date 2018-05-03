2ee78148ccdd22f5c26eb0d73837ea5c99fcb472
Benjamin Watto <wattob@aldenv179.allegheny.edu>
2018-05-02 20:44:03 -0400

    changes to caesarcipher

--- src/main/java/labten/cipher/CaesarCipher.java
+++ src/main/java/labten/cipher/CaesarCipher.java
@@ -22,6 +22,11 @@ public class CaesarCipher extends Cipher {
     StringBuffer result = new StringBuffer();
     for (int i = 0; i < source.length(); i++) {
       if (Character.isUpperCase(source.charAt(i))) {
+        char ch = (char)(((int)source.charAt(i) + shift - 65) % 26 + 65);
+        result.append(ch);
+      } else {
+        char ch = (char)(((int)source.charAt(i) + shift - 97) % 26 + 97);
+        result.append(ch);
       }
     }
     return source;
@@ -29,7 +34,7 @@ public class CaesarCipher extends Cipher {
 
   /** Demonstrate the use of the Caesar algorithm. */
   public static void main(String[] args) {
-    String[] letters = {"fortnite rulez"};
+    String[] letters = {"fortniterulez"};
     System.out.println("Before: " + java.util.Arrays.toString(letters));
     String[] sortedLetters = (new CaesarCipher()).cipher(letters);
     System.out.println("After : " + java.util.Arrays.toString(sortedLetters));
