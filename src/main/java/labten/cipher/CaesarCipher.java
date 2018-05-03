a7ab6d5ea0b221916e1c4d3f55cc81362a228e5c
Dillon Thoma <thomad@aldenv178.allegheny.edu>
2018-05-02 20:17:28 -0400

    starting CaesarCipher algorithm

--- src/main/java/labten/cipher/CaesarCipher.java
+++ src/main/java/labten/cipher/CaesarCipher.java
@@ -18,7 +18,12 @@ public class CaesarCipher extends Cipher {
    *
    *
    */
-  public String[] cipher(String[] source) {
+  public String[] cipher(String[] source, int shift) {
+    StringBuffer result = new StringBuffer();
+    for (int i = 0; i < source.length(); i++) {
+      if (Character.isUpperCase(source.charAt(i))) {
+      }
+    }
     return source;
   }
 
