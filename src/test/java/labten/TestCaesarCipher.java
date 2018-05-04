ed785207c76ff468007513f5ea40544f08e8950b
Benjamin Watto <wattob@aldenv126.allegheny.edu>
2018-05-04 13:18:07 -0400

    edits to caesar test

--- src/test/java/labten/TestCaesarCipher.java
+++ src/test/java/labten/TestCaesarCipher.java
@@ -10,47 +10,28 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.Random;
 
+/**
+ * A JUnit test suite for the CaesarCipher.
+ *
+ * @author Ben Watto and Dillon Thoma
+ */
 public class TestCaesarCipher {
 
   /** Create the object to be used in all of the test suites. */
   CaesarCipher caesarCipher = new CaesarCipher();
 
-  /** The maximum number of items to use for testing. */
-  private static int MAXIMUM_NUMBER_ITEMS = 100;
-
-  /** The maximum number of items to use for testing. */
-  private static int MAXIMUM_NUMBER_CHARS = 43;
-
-  /** The size of the alphabet for character generation. */
-  private static int ALPHABET_SIZE = 26;
-
-  /** The starting character. */
-  private static char STARTING_CHARACTER = 'a';
-
-  /** A method to determine if an array is isSorted. */
-  private boolean isSorted(char[] array) {
-    for (int i = 0; i < array.length - 1; i++) {
-      if (array[i] > array[i + 1]) {
-        return false;
-      }
-    }
-    return true;
-  }
-
   @Test
   public void testCaesarCipherA() {
     assertEquals(caesarCipher.cipher("a"), "b");
-
   }
 
   @Test
   public void testCaesarCipherB() {
     assertEquals(caesarCipher.cipher("b"), "c");
   }
-  
+
   @Test
   public void testCaesarCipherC() {
     assertEquals(caesarCipher.cipher("c"), "d");
   }
-
 }
