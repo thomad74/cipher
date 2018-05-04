233ce4e49ec33e9e90406bd53121e68abcfe1060
Dillon Thoma <thomad@aldenv127.allegheny.edu>
2018-05-04 11:51:26 -0400

    creating a test suite for results table

---
+++ src/test/java/labten/TestResultsTable.java
@@ -0,0 +1,19 @@
+package labten;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import labten.data.ResultsTable;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * A JUnit test suite for ResultsTable.
+ * @author
+ */
+
+public class TestResultsTable {
+
+}
