15e7532d190fd453ba0c867155cec4d548fbdffc
Benjamin Watto <wattob@aldenv126.allegheny.edu>
2018-05-04 12:22:35 -0400

    commented out arrays in caesarcipher

--- src/main/java/labten/cipher/CaesarCipher.java
+++ src/main/java/labten/cipher/CaesarCipher.java
@@ -56,17 +56,17 @@ public class CaesarCipher extends Cipher {
     *
     */
 
-  public String[] cipher(String[] source) {
-    int length = source.length;
-    String[] sorted = new String[source.length];
+  public String cipher(String source) {
+    //int length = source.length;
+    //String[] sorted = new String[source.length];
 
     int key = 1;
     // Makes a copy of the source array list
-    System.arraycopy(source, ARRAY_START, sorted, ARRAY_START, source.length);
+    //System.arraycopy(source, ARRAY_START, sorted, ARRAY_START, source.length);
     // iterates through the sorted array list and changes all the characters to upper case
-    for (int i = 0; i < sorted.length; i++) {
-
-    }
+    // for (int i = 0; i < sorted.length; i++) {
+    // 
+    // }
     return source;
   }
 }
